7ee8701ca5a51fffc02a0c2e9d52f669aa2a0969
Gwyn Lockett <gwyn.lockett@guardian.co.uk>
2014-06-09 17:04:00 +0100

    renamed commenting class

--- src/test/scala/com/gu/discussion/step/CommentingSteps.scala
+++ src/test/scala/com/gu/discussion/step/CommentingSteps.scala
@@ -2,31 +2,31 @@ package com.gu.discussion.step
 
 import com.gu.support.BaseSteps
 
-trait CommentSteps extends BaseSteps {
+/** Created by glockett.  */
+trait CommentingSteps extends BaseSteps{
+ 
 
-  def givenIAmSignedIn() = {
-    given(logger, "I am signed in")
-    /*driver.get("http://www.theguardian.com/")
-    //TODO get baseURL variable from config
+     def givenIAmSignedIn() = {
+      given(logger, "I am signed in")
+      /*driver.get("http://www.theguardian.com/")
+      //TODO get baseURL variable from config
+  
+      //new SignInPage(driver).signInGUDetails("it_is_me", "let_me_in")
+      //TODO get user variables from config
+  */
+    }
 
-    //new SignInPage(driver).signInGUDetails("it_is_me", "let_me_in")
-    //TODO get user variables from config
-*/
-  }
+    def whenIViewArticleComments() {
+      when(logger, "I view comments on an article")
+      /*driver.get("http://www.theguardian.com/science/grrlscientist/2012/aug/07/3")
+      //TODO get baseArticleURL variable from config
+      //new ArticlePagePage(driver).goToStartOfComments()
+      */
 
-  def whenIViewArticleComments() {
-    when(logger, "I view comments on an article")
-    /*driver.get("http://www.theguardian.com/science/grrlscientist/2012/aug/07/3")
-    //TODO get baseArticleURL variable from config
-    //new ArticlePagePage(driver).goToStartOfComments()
-    */
+    }
 
-  }
-
-  def thenICanPostAComment() {
-    then(logger, "I can post my own comment")
-  }
-
+    def thenICanPostAComment() {
+      then(logger, "I can post my own comment")
+    }
 
 }
-
